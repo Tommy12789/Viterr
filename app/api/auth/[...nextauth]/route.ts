412742efa4dc4c8c7f59c86e62690324d88abe12
Tommy <tommy@MacBook-Pro-de-Tommy.local>
2024-10-11 22:14:21 +0200

    fixing type

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,19 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/lib/prisma"; 
+import { prisma } from "@/lib/prisma";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ??"",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET??"",
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
 };
 
-
+// Next.js API Route handler
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
